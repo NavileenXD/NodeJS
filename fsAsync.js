7fae30adbba34d474efda99fbcb739f4ea51c12e
Nadine Chancay <109122203+NavileenXD@users.noreply.github.com>
2024-09-08 21:54:08 -0400

    fsAsync

---
+++ fsAsync.js
@@ -0,0 +1,11 @@
+
+const fsAsync = require('fs')
+
+//Cuando no se lee Sync, ya podemos asumir que se trata de una ejecución asíncrona
+
+fsAsync.readFile('./data/first.txt', 'utf-8', function(error, data){ //esa función es típica de javascript, y permite ejecutarse al finalizar la ejecución de este proceso
+    if(error){
+        console.log(error)
+    }
+    console.log(data)
+}) 
