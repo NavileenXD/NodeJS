eda82ab68bd677929d29d96bf632a4fcaddaa90b
Nadine Chancay <109122203+NavileenXD@users.noreply.github.com>
2024-09-20 09:42:27 -0400

    new commit Aync Await (better use of Promise)

---
+++ asyncAwait.js
@@ -0,0 +1,32 @@
+const {readFile}= require('fs')  
+
+
+const getText = (pathFile) => { //Función con promesa
+    return new Promise(function(resolve, reject){
+        readFile(pathFile, 'utf-8', (err, data) => {
+            if (err){
+                reject(err)
+            }
+            resolve(data)
+        })
+    })
+}
+
+
+
+//Para no usar la función y colocar .then y .catch en todo momento tenemos async-await
+
+async function read() {
+    try {
+        const result = await getText("./data/first.txt");
+        console.log(result);
+        const result2 = await getText("./data/second.txt");
+        console.log(result2);
+        const result3 = await getText('./data/fourth.txt');
+        console.log(result3);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+read()
